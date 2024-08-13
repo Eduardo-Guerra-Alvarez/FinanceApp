fdddfc0208f5f2c6fab90b52334408602815e628
Eduardo Guerra Alvarez <lalo52rojo@gmail.com>
2024-08-12 19:33:51 -0600

    add 1 in month to get correct data

--- app/src/main/java/com/finance/finance_app/DatePickerFragment.kt
+++ app/src/main/java/com/finance/finance_app/DatePickerFragment.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.Context
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
 
@@ -18,7 +19,7 @@ class DatePickerFragment(val listener: (day:Int, month:Int, year:Int) -> Unit):D
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val calendar = Calendar.getInstance()
         val day = calendar.get(Calendar.DAY_OF_MONTH)
-        val month = calendar.get(Calendar.MONTH)
+        val month = calendar.get(Calendar.MONTH)+1 // month start in 0 that is why we add 1
         val year = calendar.get(Calendar.YEAR)
 
         val picker = DatePickerDialog(activity as Context, this, year, month, day)
