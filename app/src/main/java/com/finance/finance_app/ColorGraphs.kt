6343d5d79ec9729bbbd6e847771b05c2e3ce1893
Eduardo Guerra Alvarez <lalo52rojo@gmail.com>
2024-08-26 18:43:22 -0600

    add object colors

---
+++ app/src/main/java/com/finance/finance_app/ColorGraphs.kt
@@ -0,0 +1,20 @@
+package com.finance.finance_app
+
+import android.graphics.Color
+
+object ColorGraphs {
+    val colors = listOf(
+        Color.parseColor("#B29462"),
+        Color.parseColor("#95A5A6"),
+        Color.parseColor("#a0afa1"),
+        Color.parseColor("#92ba92"),
+        Color.parseColor("#008585"),
+        Color.parseColor("#DD6E42"),
+        Color.parseColor("#E8DAB2"),
+        Color.parseColor("#C0D6DF"),
+        Color.parseColor("#EAEAEA"),
+        Color.parseColor("#344966"),
+        Color.parseColor("#BFCC94"),
+        Color.parseColor("#C2EFB3")
+    )
+}
